44f7aad8c2ff272586a6590f802f9751b6ae7e52
fayereniouuu-art <fayeren.iouuu@gmail.com>
2026-03-01 00:19:47 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,23 +1,24 @@
 const mysql = require("mysql");
-require('dotenv').config()
+require('dotenv').config();
 
 const con = mysql.createPool({
     connectionLimit : 20, 
-    host: 'mysql-d5b161e-hosapi123.j.aivencloud.com', //
-    port: 10306, //
-    user: 'avnadmin', //
-    password: 'AVNS_gJv8rYDINF4SvFPus4E', //
-    database: 'defaultdb', //
+    host: process.env.MYSQL_HOST,  
+    port: process.env.MYSQL_PORT || 10306, 
+    user: process.env.MYSQL_USER, 
+    password: process.env.MYSQL_PASSWORD, 
+    database: process.env.MYSQL_DATABASE || 'defaultdb', 
     ssl: {
-        rejectUnauthorized: false
-    }, 
+        rejectUnauthorized: false // ⭐ จำเป็นมากสำหรับการต่อ Aiven จากภายนอก
+    },
+    charset: 'utf8mb4',
     timezone: '+07:00', 
     dateStrings: true   
 });   
 
 con.getConnection(function(err, connection) {
     if (err) {
-        console.error('ERP Error connecting : '+err.stack);
+        console.error('ERP Error connecting : ' + err.stack);
         return;
     } 
     console.log('Database connected successfully!'); 
