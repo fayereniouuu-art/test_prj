05294b7171c3847956c2fc7c0a01f8cb98cd4fa9
fayereniouuu-art <fayeren.iouuu@gmail.com>
2026-03-01 00:17:02 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,15 +2,17 @@ const mysql = require("mysql");
 require('dotenv').config()
 
 const con = mysql.createPool({
-    connectionLimit : 20, // แนะนำให้เปิดไว้ (กำหนดจำนวนการเชื่อมต่อพร้อมกันสูงสุด)
-    host: process.env.MYSQL_HOST,  
-    user: process.env.MYSQL_USER, 
-    password: process.env.MYSQL_PASSWORD, 
-    database: process.env.MYSQL_DATABASE, 
-    charset: process.env.MYSQL_CHARSET,
-    port: process.env.MYSQL_PORT,
-    timezone: '+07:00', // ⭐ เพิ่มบรรทัดนี้ เพื่อตั้งให้ Node.js บันทึก/อ่านเวลาเป็นเวลาประเทศไทยเสมอ
-    dateStrings: true   // ⭐ เพิ่มบรรทัดนี้ (Option) เพื่อให้เวลาที่ Query ออกมาเป็น String อ่านง่าย ไม่เพี้ยน
+    connectionLimit : 20, 
+    host: 'mysql-d5b161e-hosapi123.j.aivencloud.com', //
+    port: 10306, //
+    user: 'avnadmin', //
+    password: 'AVNS_gJv8rYDINF4SvFPus4E', //
+    database: 'defaultdb', //
+    ssl: {
+        rejectUnauthorized: false
+    }, 
+    timezone: '+07:00', 
+    dateStrings: true   
 });   
 
 con.getConnection(function(err, connection) {
@@ -18,7 +20,7 @@ con.getConnection(function(err, connection) {
         console.error('ERP Error connecting : '+err.stack);
         return;
     } 
-    console.log('Database connected successfully!'); // ลองใส่ log ไว้ดูว่าต่อ DB สำเร็จไหม
+    console.log('Database connected successfully!'); 
     connection.release();
 });
 
