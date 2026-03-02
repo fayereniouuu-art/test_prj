81438579e2e39764e8945c235f988eab7ccd49b7
fayereniouuu-art <fayeren.iouuu@gmail.com>
2026-03-03 00:39:48 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -142,22 +142,21 @@ router.delete('/building/:id', verifyToken, isAdmin, async (req, res) => {
 
 router.get('/reference_points', async (req, res) => {
     try {
-        // อัปเดต Subquery เพื่อเช็คจากตาราง route ด้วย start_building_id หรือ end_building_id
+        // เอา latitude, longitude ออกแล้ว
         const sqlQuery = `
-    SELECT 
-        rp.reference_points_id,
-        rp.building_id,
-        b.building_name, 
-        rp.corner1_coord_x, rp.corner1_coord_y,
-        rp.corner2_coord_x, rp.corner2_coord_y,
-        rp.corner3_coord_x, rp.corner3_coord_y,
-        rp.corner4_coord_x, rp.corner4_coord_y,
-        rp.building_image_path,
-        (SELECT COUNT(*) FROM \`route\` WHERE \`route\`.start_building_id = rp.building_id OR \`route\`.end_building_id = rp.building_id) > 0 AS has_route
-    FROM \`reference_points\` rp
-    JOIN \`building\` b ON rp.building_id = b.building_id
-    ORDER BY b.building_name ASC
-`;
+            SELECT 
+                rp.reference_points_id,
+                rp.building_id,
+                b.building_name, 
+                rp.corner1_coord_x, rp.corner1_coord_y,
+                rp.corner2_coord_x, rp.corner2_coord_y,
+                rp.corner3_coord_x, rp.corner3_coord_y,
+                rp.corner4_coord_x, rp.corner4_coord_y,
+                rp.building_image_path 
+            FROM \`reference_points\` rp
+            JOIN \`building\` b ON rp.building_id = b.building_id
+            ORDER BY b.building_name ASC
+        `;
         const results = await query(sqlQuery);
 
         return res.status(200).json({
@@ -515,4 +514,3 @@ router.put('/reference_point/:id', verifyToken, isAdmin, async (req, res) => {
 });
 
 module.exports = router;
-
